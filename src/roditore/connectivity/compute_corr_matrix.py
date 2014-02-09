07fb06bb0de8a3bb5822eb82af5e562b19813c9a
dobryden <adam.liska@studenti.unitn.it>
2014-02-09 17:29:57 +0100

    correlation matrices saved as HDF5

--- src/roditore/connectivity/compute_corr_matrix.py
+++ src/roditore/connectivity/compute_corr_matrix.py
@@ -2,24 +2,19 @@
 
 import argparse
 import nibabel as nib
+import h5py
 import numpy as np
 from roditore.utils.image_utils import voxel_coord_gen
 
-def compute_corr_matrix(imagefile, maskfile, output=None, index=None):
+def compute_corr_matrix(imagefile, maskfile):
     img = nib.load(imagefile).get_data()
     mask = nib.load(maskfile).get_data()
 
     voxels = [pos for pos in voxel_coord_gen(mask.shape, mask=mask)]
     corrmat = np.corrcoef([img[pos] for pos in voxels])
 
-    if output != None:
-        np.save(output, corrmat)
-    if index != None:
-        with open(index, 'w') as f:
-            for voxel in voxels:
-                f.write('{0[0]} {0[1]} {0[2]}\n'.format(voxel))
+    return corrmat, voxels
 
-    return corrmat
 
 def compute_corr_matrix_argparse():
     parser = argparse.ArgumentParser(description=('Computes correlation '
@@ -29,6 +24,8 @@ def compute_corr_matrix_argparse():
     parser.add_argument('-o', '--output', metavar='OUTPUT',
             required=True, 
             help='Correlation matrix will be written to OUTPUT')
+    parser.add_argument('-d', '--dataset', metavar='DATASETNAME',
+            required=True, help='Name of the HDF5 dataset.')
     parser.add_argument('-x', '--index', metavar='INDEXFILE',
             help='The index file will be output to INDEXFILE.')
     parser.add_argument('-m', '--mask', metavar='MASK',
@@ -37,8 +34,16 @@ def compute_corr_matrix_argparse():
 
 def main():
     args = compute_corr_matrix_argparse().parse_args()
-    compute_corr_matrix(args.input, args.mask, output=args.output, 
-            index=args.index)
+    corrmat, voxels = compute_corr_matrix(args.input, args.mask)
+
+    f = h5py.File(args.output, 'w-')
+    f.create_dataset(args.dataset, data=corrmat)
+    f.close()
+
+    if args.index != None:
+        with open(args.index, 'w') as f:
+            for voxel in voxels:
+                f.write('{0[0]} {0[1]} {0[2]}\n'.format(voxel))
 
 if __name__ == "__main__":
     main()
