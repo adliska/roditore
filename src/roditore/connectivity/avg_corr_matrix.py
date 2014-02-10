8fcaaf0dc591d88aaf8b910e5baa05b885cb2bc5
dobryden <adam.liska@studenti.unitn.it>
2014-02-10 11:06:36 +0100

    avg_corr_mat outputs to an HDF5 dataset

--- src/roditore/connectivity/avg_corr_matrix.py
+++ src/roditore/connectivity/avg_corr_matrix.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import h5py
 import numpy as np
 import sys
 
@@ -9,14 +10,18 @@ def avg_corr_matrix(matfiles, verbose=False):
         print 'Total number of matrices:', str(len(matfiles))
         print '1..'
 
-    subjmat = np.load(matfiles[0])
+    f = h5py.File(matfiles[0], 'r')
+    subjmat = f['corrmat'][...]
+    f.close()
     np.fill_diagonal(subjmat, 0)
     avgmat = np.arctanh(subjmat)
 
     for i in xrange(1, len(matfiles)):
         if verbose:
             print str(i+1)+'..'
-        subjmat = np.load(matfiles[i])
+        f = h5py.File(matfiles[i], 'r')
+        subjmat = f['corrmat'][...]
+        f.close()
         np.fill_diagonal(subjmat, 0)
         avgmat = avgmat + np.arctanh(subjmat)
 
@@ -33,13 +38,20 @@ def avg_corr_matrix_argparser():
     parser.add_argument('-o', '--output', metavar='AVGMAT', 
             help='The averaged correlation matrix will be written to AVGMAT.',
             required=True)
+    parser.add_argument('-d', '--dataset', metavar='DATASET',
+            help='Name of the HDF5 dataset.', required=True)
     parser.add_argument('-v', '--verbose', help='switch on the verbose mode',
             action='store_true', default=False)
     return parser
 
 def main():
     args = avg_corr_matrix_argparser().parse_args()
-    np.save(args.output, avg_corr_matrix(args.input, verbose=args.verbose))
+
+    avgcorrmat = avg_corr_matrix(args.input, verbose=args.verbose)
+
+    f = h5py.File(args.output, 'w-')
+    f.create_dataset(args.dataset, data=avgcorrmat)
+    f.close()
 
 if __name__ == "__main__":
     main()
