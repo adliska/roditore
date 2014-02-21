46abf154bace75e4f126eb1a08d8dd95cc4c0384
dobryden <adam.liska@studenti.unitn.it>
2014-02-21 10:21:50 +0100

    add: correlation matrix subsetting

--- src/roditore/connectivity/subset_correlation_matrix.py
+++ src/roditore/connectivity/subset_correlation_matrix.py
@@ -5,49 +5,44 @@ import h5py
 import argparse
 import roditore.utils.image_utils as imutils
 
-def remove_voxels(corrmat, index, remove):
-    indices = [i for i,j in enumerate(index) if j in remove]
+def subset_correlation_matrix(corrmat, index, subset):
+    indices = [index.index(pos) for pos in subset]
+    return corrmat[np.ix_(indices, indices)]
 
-    corrmat = np.delete(corrmat, indices, 0) ;
-    corrmat = np.delete(corrmat, indices, 1) ;
-
-    return corrmat
-    
-
-def remove_voxels_argparser():
+def subset_correlation_matrix_argparser():
     parser = argparse.ArgumentParser(
-            description=('Removes voxels specified by dimensions.'))
+            description=('Subsets a correlation matrix.'))
     parser.add_argument('input', metavar='INPUT', help='Input HDF5 file')
-    parser.add_argument('-d', '--dataset', metavar='DATASET',
+    parser.add_argument('subset', metavar='subset', help='List of voxels of '
+            'interest.')
+    parser.add_argument('-d', '--dataset', metavar='dataset',
             help='Dataset with the correlation matrix (for both input '
             'and output.', required=True)
-    parser.add_argument('-x', '--index', metavar='INDEX',
+    parser.add_argument('-x', '--index', metavar='index',
             help='The voxel index file', required=True)
-    parser.add_argument('-r', '--remove', metavar='REMOVE',required=True,
-            help='List of voxels to remove')
     parser.add_argument('-o', '--output', metavar='OUTPUT',
             help='Output HDF5 file', required=True)
     
     return parser
 
 def main():
-    args = remove_voxels_argparser().parse_args()
+    args = subset_correlation_matrix_argparser().parse_args()
 
     print 'Reading input dataset.'
     f = h5py.File(args.input, 'r')
     corrmat = f[args.dataset][...]
     f.close()
-    
     print 'Total number of voxels:', str(corrmat.shape[0])
 
     print 'Reading voxel index.'
     index = imutils.voxel_index(args.index)
 
-    print 'Reading voxels to remove.'
-    remove = imutils.voxel_index(args.remove)
+    print 'Reading the voxels subset.'
+    subset = imutils.voxel_index(args.subset)
+    print 'Total number of voxels in the subset:', str(len(subset))
 
-    print 'Removing voxels.'
-    output = remove_voxels(corrmat, index, remove)
+    print 'Subsetting the correlation matrix.'
+    output = subset_correlation_matrix(corrmat, index, subset)
 
     print 'Writing output.'
     f = h5py.File(args.output, 'w-')
